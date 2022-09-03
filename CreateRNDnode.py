07acaf313ae6adff9273763dd32ab02e97669519
Tronotrond <tronotrond@gmail.com>
2022-09-03 13:19:17 -0500

    Updated script..
    Added support for selecting non null or output nodes. 
    If so an null will be created for the selected node. 
    
    Changed from using root GEO node's name as default to a custom name dialog box. 
    If a null or output node is selected, the name of this node is used to pre-populate the dialog box.
    
    Added additional error checks. 
    Cleaned up the code a bit

--- CreateRNDnode.py
+++ CreateRNDnode.py
@@ -1,49 +1,85 @@
 import hou
 
 acceptedNodeTypes = ('null', 'output')
-removePrefixes = ('ASSET_', 'GEO_', 'FX_')
+removePrefixes = ('ASSET_', 'GEO_', 'FX_', 'ANIM_', 'OUT_', 'RND_')
 
-nodes = hou.selectedNodes()
-node = nodes[0]
 
 def RemPrefix(text, prefixes):
+    # Remove prefix from given string
     for pf in prefixes:
         if text.startswith(pf):
             return text[len(pf):]
     
     return text
-
+    
 
 def GetNodeByDialog():
+    # Opens a dialog box for user to select node
     n = hou.ui.selectNode(title='Select Output or Null', node_type_filter=hou.nodeTypeFilter.Sop)
     node = hou.node(n)
     if node is None:
         return None
-    if node.type() in acceptedNodeTypes:
+    if node.type().name() in acceptedNodeTypes:
         return node
         
     return None
 
-if len(nodes) != 1:
-    node = GetNodeByDialog()
-else:
-    t = nodes[0].type()
-    if t.name() not in acceptedNodeTypes:
-        node = GetNodeByDialog() 
-
-        
-if node is not None:
-    root = node.parent()
-    name = RemPrefix(root.name(), removePrefixes)
-    name = 'RND_' + name
     
+def AskForName(initial_name=''):
+    input = hou.ui.readInput('Render node name', initial_contents=initial_name, close_choice=1, buttons=('OK', 'Cancel'))
+    print(input)
+    if(input == None or input[0] != 0):
+        return None
+        
+    return input[1]
+    
+    
+def CreateRenderNode(node):
+            
+    root = node.parent()
+    name = 'RND_'
+    # Get name of root object - used before
+    #name = RemPrefix(root.name(), removePrefixes)
+    
+    # If user selected ouput/null, get name and use this node for object merge
+    # if not, create a null and name it correctly
+    if node.type().name() in acceptedNodeTypes:
+        orgname = node.name()
+        name += RemPrefix(node.name(), removePrefixes)
+        name = AskForName(name)
+        
+        # Check if user cancelled input
+        if(name == None): 
+            return
+    else:
+        name = AskForName(name)
+        # Check if user cancelled input
+        if(name == None): 
+            return
+        
+        # Create new NULL
+        nullname = 'OUT_' + RemPrefix(name, removePrefixes)
+        nullnode = root.createNode('null', node_name=nullname)
+        nullnode.setInput(0, node)
+        nullnode.moveToGoodPosition()
+        node = nullnode
+    
+    # Create the render GEO node
     root = root.parent() # Get to base level
-    rnd = root.createNode('geo', name)
+    rnd = root.createNode('geo', name, force_valid_node_name=1)
     rnd.setColor(hou.Color(1.0, 0.0, 0.0))
+    rnd.moveToGoodPosition()
     
     om = rnd.createNode('object_merge')
     parm = om.parm('objpath1')
     parm.set(node.path())
+
+        
+nodes = hou.selectedNodes()
+if len(nodes) == 1:
+    if nodes[0].type().category() == hou.sopNodeTypeCategory():
+        CreateRenderNode(nodes[0])
+    else:
+        print('Tool only work on SOP level nodes')
 else:
-    hou.ui.displayMessage('No valid Output or NULL node selected!')
-    
+    print('Select single node...')
