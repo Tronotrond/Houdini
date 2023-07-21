37cac7e60c6189495543e4c6033c7ee4a557e3e0
Tronotrond <tronotrond@gmail.com>
2023-07-21 15:19:14 -0500

    Update GenerateMaterials.py
    Added a basic UI and support for multiple render engines / material types. 
    
    Flexible code to add more options later.  
    
    Added support for generating Karma Material X

--- GenerateMaterials.py
+++ GenerateMaterials.py
@@ -1,6 +1,6 @@
 ######################################################
 ##
-##  Generate materials from shop_materialpath v1.0
+##  Generate materials from shop_materialpath v1.1
 ##  Trond Hille 2023
 ##
 ##  FBX and external models often come in with material paths set, but recreating the materials is a manual process.
@@ -8,16 +8,91 @@
 ##
 ##  Currently defaults to creating a redshift vopnet (material builder) but can easily be changed to another type.
 ##
+##  Changelog:
+##  v1.0    initial release. Redshift Materials Only
+##  v1.1    added support and choice for different render engines/material types 
+##                Added support for Karma MaterialX and ease to add more later
+##
+##
 ##
 #####################################################
 
 
 import hou
 import re
+from enum import Enum
 
 materials = [] 
 
-def CreateMaterial(matnet, matname, mattype='redshift_vopnet', skipexisting='True'):
+
+class ExtendedEnum(Enum):
+
+    @classmethod
+    def list(cls):
+        return list(map(lambda c: c.name, cls))
+
+
+class MatType(ExtendedEnum):
+    Redshift = "redshift_vopnet"
+    KarmaMtlX = "karmamtlx_vopnet"
+    
+    
+    
+# SHOW MAIN DIALOG    
+def showDialog(node):
+    # List of options for the dropdown list
+    dropdown_options = MatType.list()
+
+    
+    # Create the custom dialog with the dropdown menu
+    result = hou.ui.selectFromList(dropdown_options, exclusive=True, title='Select Material Type', message='Select Material Type', clear_on_cancel=True)
+    if len(result) != 0:
+        matclass = getattr(MatType, dropdown_options[result[0]])
+        #print(matclass)
+        GenerateMaterialsFromGeo(node, matclass)
+
+        
+        
+        
+
+def CreateKarmaMtlx(matnet, matname):
+    matvopnet = matnet.createNode('subnet', matname)
+    mtlx = matvopnet.createNode('mtlxstandard_surface', matname)
+    
+    # CLEANUP Default nodes
+    tempPath = matvopnet.path() + '/suboutput1'
+    defOutput = hou.node(tempPath)
+    tempPath = matvopnet.path() + '/subinput1'
+    defInput = hou.node(tempPath)
+    defOutput.destroy()
+    defInput.destroy()
+
+    surfaceoutput = matvopnet.createNode('subnetconnector', 'surface_output')
+        
+    surfaceoutput.parm('connectorkind').set(1)
+    surfaceoutput.parm('parmname').set('surface')
+    surfaceoutput.parm('parmlabel').set('Surface')
+    surfaceoutput.parm('parmtype').set(24) #Set to surface type
+    surfaceoutput.parm('connectorkind').eval()
+    surfaceoutput.setInput(0, mtlx)
+    
+    pos = mtlx.position()
+    pos += hou.Vector2((4,0))
+    surfaceoutput.setPosition(pos)
+    pos += hou.Vector2((0,-4))
+    
+    displacementoutput = matvopnet.createNode('subnetconnector', 'displacement')
+    displacementoutput.parm('connectorkind').set(1)
+    displacementoutput.parm('parmname').set('displacement')
+    displacementoutput.parm('parmlabel').set('Displacement')
+    displacementoutput.parm('parmtype').set(25) #Set to displacement type
+    displacementoutput.parm('connectorkind').eval()
+    displacementoutput.setPosition(pos)
+        
+    return matvopnet
+    
+
+def CreateMaterial(matnet, matname, mattype=MatType.Redshift, skipexisting='True'):
     #Creates a new material at the given matnet. Defaults to Redshift Material Builder type.
     
     if skipexisting:
@@ -25,8 +100,12 @@ def CreateMaterial(matnet, matname, mattype='redshift_vopnet', skipexisting='Tru
         if hou.node(matpath) is not None:
             print(matname + ' already exists. Skipping Creation..')
             return hou.node(matpath)
-    
-    newmat = matnet.createNode(mattype, matname)
+    newmat = None
+    if mattype is MatType.KarmaMtlX:
+        newmat = CreateKarmaMtlx(matnet, matname)
+    else:
+        newmat = matnet.createNode(mattype.value, matname)
+        
     return newmat
 
 def remove_illegal_characters(string):
@@ -51,7 +130,7 @@ def CollectMaterialNames(node):
         
 
 
-def GenerateMaterialsFromGeo(node):
+def GenerateMaterialsFromGeo(node, mattype=MatType.Redshift):
     # Main script execution
     
     root = node.parent()
@@ -72,7 +151,7 @@ def GenerateMaterialsFromGeo(node):
         matname = mat.rsplit('/', 1)[-1]
         matname = remove_illegal_characters(matname)
         
-        newmat = CreateMaterial(matnet, matname)
+        newmat = CreateMaterial(matnet, matname, mattype)
             
         newmat.moveToGoodPosition()
         newmats.append(newmat)
@@ -117,4 +196,4 @@ if len(hou.selectedNodes()) != 1:
     print('Select one SOP node with shop_materialpath set')
 else:
     node = hou.selectedNodes()[0]
-    GenerateMaterialsFromGeo(node)
+    showDialog(node)
